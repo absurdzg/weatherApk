4b83c4b2d10aea0979600810e555fc7d303ea42a
Andrzej <zapala333@gmail.com>
2017-06-14 11:23:26 +0200

    aplikacja działajaca i ładnie wygladajaca

---
+++ src/main/java/weather/network/Timer.java
@@ -0,0 +1,41 @@
+package weather.network;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import weather.controller.WeatherAppController;
+
+public class Timer
+{
+    private WeatherAppController controller;
+    private static final int REFRESH_TIME = 2 * 60 * 1000; //time in millis
+    private Timeline timeline;
+
+
+    public Timer(WeatherAppController controller)
+    {
+        timeline = new Timeline();
+        this.controller = controller;
+    }
+
+    public void startTimer()
+    {
+        if (timeline.getStatus().equals(Animation.Status.RUNNING)) //nie nakladamy na siebie dwoch timerow
+        {
+            timeline.stop();
+        }
+
+        timeline = new Timeline(new KeyFrame(
+                Duration.millis(REFRESH_TIME),
+                ae -> controller.refreshButtonClicked()));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    public void stopTimer()
+    {
+        timeline.stop();
+    }
+
+}
